ee4637cb92e7e29f977a6e632adfd556b1bb7e97
Krzysztof <krzysztof.kolodzinski@lifeflow.eu>
2023-01-05 15:07:26 +0100

    changed test for real world

--- tests/e2e_tests/cypress.config.js
+++ tests/e2e_tests/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://www.goodmap.localhost:5000/',
+    baseUrl: 'http://www.staging.rzutberetem.eco/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
